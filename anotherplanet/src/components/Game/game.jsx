3439e7f03bb4d878d31049b2a7fe6a5434176c32
Amalia <amalia@AirsomtorAmalia.lan>
2022-01-03 14:27:44 +0100

    updating text

--- anotherplanet/src/components/Game/game.jsx
+++ anotherplanet/src/components/Game/game.jsx
@@ -7,12 +7,16 @@ const Game = () => {
   return (
     <Container className="page-container">
       <Col>
-        <Row className="page-title"
-        style={{
+        <Row
+          className="page-title"
+          style={{
             backgroundImage: `url("https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/7.11-400d1709madagascar.jpg")`,
             backgroundSize: "cover",
-          }}>The Game</Row>
-        
+          }}
+        >
+          The Game
+        </Row>
+
         <Row className="page-header">Technologies</Row>
         <Row className="page-header2">Graphics</Row>
         <Col className="page-text">
@@ -39,6 +43,18 @@ const Game = () => {
           including versions of Lorem Ipsum.
         </Col>
         <Row className="page-header2">Challenges</Row>
+        <Col className="page-text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </Col>
+        <Row className="page-header2">Lessons learned</Row>
         <Col className="page-text">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
--- anotherplanet/src/components/Home/home.jsx
+++ anotherplanet/src/components/Home/home.jsx
@@ -9,59 +9,72 @@ const Home = ({ ...props }) => {
       <Col>
         <Row className="page-title">Another Planet</Row>
         <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/FYOH_54XEJY"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          src="https://drive.google.com/file/d/1JoLNgHdvI4um27DcYCuQmhH4Kjs-vSW5/preview"
+          width="640"
+          height="480"
+          allow="autoplay"
         ></iframe>
         <Row className="page-header">Description</Row>
         <Col className="page-text">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          Another Planet is a VR project built in the course DH2413 Advanced
+          Graphics and Interaction at KTH Royal Institute of Technology. The
+          purpose of the project is to educate children in the age 13-15 about
+          gravity on different planets. In the immersive and interactive VR game
+          they can walk, jump, throw and investigate items, in order to learn
+          more about how the different gravities affect them. The project was
+          built in Unreal, and used HTC Vive controls.
         </Col>
         <Row className="page-header">Goals and Motivation</Row>
         <Col className="page-text">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          Firstly, we wanted to learn about Unreal, since none of us had worked
+          with it before and we wanted to try out another game engine than
+          Unity. Secondly, we also wanted to create an educational environment
+          where play and curiosity is key, hence that there is no strict game
+          design prompting the user to do certain things and the game platforms
+          on certain planets shift layout. Lastly, we ourselves wanted to learn
+          more about space and how to design space in 3D environments.
         </Col>
         <Row className="page-header">Previous works</Row>
-        <Row className="page-header2">Previous work 1</Row>
+        <Row className="page-header2">
+          <a href="https://ieeexplore.ieee.org/document/5561320">
+            Aw Kien Sin, and Halimah Badioze Zaman - "Live Solar System (LSS):
+            Evaluation of an Augmented Reality book-based educational tool."
+          </a>
+        </Row>
         <Col className="page-text">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+          In this paper, the authors explore how the astronomy education book
+          Live Solar System (LSS), enhanced through augumented reality, can be
+          affected by using tangible tools to augment the AR further. What we
+          learned from this paper is that good graphics are very important when
+          designing space environments, and that tangibility can have a positive
+          effect on the learning experience.
         </Col>
-        <Row className="page-header2">Previous work 2</Row>
+        <Row className="page-header2">
+          <a href="https://gupea.ub.gu.se/bitstream/2077/39977/1/gupea_2077_39977_1.pdf">
+            Mustafa Hussein, and Carl Nätterdal - "The Benefits of Virtual
+            Reality in Education- A comparision Study"
+          </a>
+        </Row>
         <Col className="page-text">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+          Hussein and Nätterdal reviews in this paper a VR astronomy learning
+          application. They found that VR is effective where interaction is
+          important for educational measures, and that it helps students become
+          immersed and involved in the learning experience, as well as that it
+          promotes active learning.
         </Col>
-        <Row className="page-header2">Previous work 3</Row>
+        <Row className="page-header2">
+          <a href="https://www.learntechlib.org/p/9543/">
+            Yoav Yair, Rachel Mintz and Shai Litvak - "3D-Virtual Reality in
+            Science Education: An Implication for Astronomy Teaching"
+          </a>
+        </Row>
         <Col className="page-text">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+          How can we become more heliocentric, instead of geocentric? In this
+          paper, the authors present a virtual 3D environment which teaches the
+          user about the solar system by making it possible to explore it from
+          different angles and views. This early work on 3D, exploration and
+          education showed that it can help with mentally constructing 3D space
+          and learning about physical principles.
         </Col>
       </Col>
     </Container>
